e4723660f3a10e4f15a4a21e9698dea3c413cd8c
Dominick <dominickfrench.oe@gmail.com>
2025-10-01 14:13:29 -0500

    Second commit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("Enter the race result (Format: Home:Visitor|HomeScore:VisitorScore): ");
+        String input = myScanner.nextLine().trim();
+
+        String[] sections = input.split("\\|");
+
+
+
+    }
 }
